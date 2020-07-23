dc8212803ad43982601323f340b2c3fc6c0d40b2
jezeraskicm <jezeraskicm@gitlab.orfo.de>
2020-07-23 13:48:45 +0200

    Update Notification is implemented.

--- src/index.js
+++ src/index.js
@@ -10,8 +10,29 @@ import * as serviceWorker from './serviceWorker';
 
 
 const App = () => {
+  const [newVersionAvailable, setNewVersionAvailable] = React.useState(false);
+  const [waitingWorker, setWaitingWorker] = React.useState(null);
+
+
+  const onSWUpdate = (registration) => {
+    setNewVersionAvailable(true);
+    setWaitingWorker(registration.waiting);
+  };
+
+  React.useEffect(() => {
+    serviceWorker.register({ onUpdate: onSWUpdate });
+  }, []);
+
+  const reloadPage = () => {
+    waitingWorker && waitingWorker.postMessage({ type: 'SKIP_WAITING' });
+    setNewVersionAvailable(false);
+    window.location.reload(true);
+  };
+
   return (
     <BrowserRouter>
+      {newVersionAvailable && <button onClick={reloadPage} className="uploadBtn">Reload</button>}
+
       <Route exact path="/" component={HomeView} />
       <Route exact path="/form" component={FormView} />
     </BrowserRouter>
@@ -20,4 +41,4 @@ const App = () => {
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
-serviceWorker.register();
+// serviceWorker.register();
